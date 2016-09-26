ef635e72785e2d13dc3a22d1e2fc6aec73eeee23
George Gecewicz <gecewicz.george@gmail.com>
2016-09-26 18:11:07 -0400

    Update and rename order-events-by-publish-date-in-event-list-widget.php to plugin.php

--- plugin.php
+++ plugin.php
@@ -1,29 +1,74 @@
 <?php
 /**
- * Plugin Name: The Events Calendar — Order Events by Publish Date in Event List Widget
+ * Plugin Name: The Events Calendar Extension: Order Events by Publish Date in Event List Widget
  * Description: Make list widgets display events in the order they were published instead of the default "upcoming events" order.
  * Version: 1.0.0
  * Author: Modern Tribe, Inc.
- * Author URI: http://m.tri.be/1x
+ * Author URI: http://m.tri.be/1971
  * License: GPLv2 or later
  */
- 
+
 defined( 'WPINC' ) or die;
 
-class Tribe__Order_List_Widget_Events_by_Publish_Date {
-	
-	protected $constraints = array(
+class Tribe__Extension__Order_List_Widget_Events_by_Publish_Date {
+
+    /**
+     * The semantic version number of this extension; should always match the plugin header.
+     */
+    const VERSION = '1.0.0';
+
+    /**
+     * Each plugin required by this extension
+     *
+     * @var array Plugins are listed in 'main class' => 'minimum version #' format
+     */
+    public $plugins_required = array(
+        'Tribe__Events__Main' => '4.2'
+    );
+
+    /**
+     * Parameters that let you target specific widget instances.
+     *
+     * @var array
+     */
+    protected $constraints = array(
 		'sidebar_id'   => null,
 		'widget_id'    => null,
 		'widget_title' => null
 	);
 
-	public function __construct( array $constraints = array() ) {
-		$this->constraints = array_merge( $this->constraints, $constraints );
-		add_filter( 'widget_display_callback', array( $this, 'setup' ), 10, 3 );
-	}
+    /**
+     * The constructor; delays initializing the extension until all other plugins are loaded.
+     */
+    public function __construct( array $constraints = array() ) {
 
-	public function setup( $instance, $widget, $args ) {
+    	$this->constraints = array_merge( $this->constraints, $constraints );
+
+        add_action( 'plugins_loaded', array( $this, 'init' ), 100 );
+    }
+
+    /**
+     * Extension hooks and initialization; exits if the extension is not authorized by Tribe Common to run.
+     */
+    public function init() {
+
+        // Exit early if our framework is saying this extension should not run.
+        if ( ! function_exists( 'tribe_register_plugin' ) || ! tribe_register_plugin( __FILE__, __CLASS__, self::VERSION, $this->plugins_required ) ) {
+            return;
+        }
+
+        add_filter( 'widget_display_callback', array( $this, 'setup' ), 10, 3 );
+    }
+
+    /**
+     * Set up the ordering overwrites on widget instances.
+     *
+     * @param array $instance
+     * @param object $widget
+     * @param array $args
+     * @return array
+     */
+    public function setup( $instance, $widget, $args ) {
 		
 		// We're interested only in the (advanced or regular) events list widgets.
 		$targets = array( 'tribe-events-adv-list-widget', 'tribe-events-list-widget' );
@@ -41,6 +86,13 @@ class Tribe__Order_List_Widget_Events_by_Publish_Date {
 		return $instance;
 	}
 
+	/**
+     * For the given widget instance, see if it meets the constraints supplied to target specific widgets.
+     *
+     * @param array $instance
+     * @param array $args
+     * @return array
+     */
 	protected function constraints_met( $instance, $args ) {
 		$fail = false;
 
@@ -59,6 +111,12 @@ class Tribe__Order_List_Widget_Events_by_Publish_Date {
 		return ! $fail;
 	}
 
+	/**
+     * Set up the alteration of the orderby parameters on allowed widget instances.
+     *
+     * @param array $args
+     * @return array
+     */
 	public function order_by_latest( $args ) {
 
 		// Don't interfere in other queries.
@@ -70,6 +128,12 @@ class Tribe__Order_List_Widget_Events_by_Publish_Date {
 		return $args;
 	}
 
+	/**
+     * Perform the alteration of the orderby parameters on allowed widget instances.
+     *
+     * @param array $args
+     * @return array
+     */
 	public function override_orderby( $orderby_sql ) {
 		global $wpdb;
 
@@ -90,16 +154,15 @@ class Tribe__Order_List_Widget_Events_by_Publish_Date {
  *   - widget_title: if you don't know how to determine the widget/sidebar ID, you can specify the widget title.
  *
  * Example 1:
- *    new Tribe__Order_List_Widget_Events_by_Publish_Date( array(
+ *    new Tribe__Extension__Order_List_Widget_Events_by_Publish_Date( array(
  *        'sidebar_id' => 'sidebar-1'
  *    ) );
  *
  * Example 2:
- *    new Tribe__Order_List_Widget_Events_by_Publish_Date( array(
+ *    new Tribe__Extension__Order_List_Widget_Events_by_Publish_Date( array(
  *        'widget_title' => 'Newly Added Events!'
  *    ) );
  *
  * Go to http://m.tri.be/194x for more information.
  */
-
-new Tribe__Order_List_Widget_Events_by_Publish_Date();
+new Tribe__Extension__Order_List_Widget_Events_by_Publish_Date();

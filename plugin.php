106510d291d99d5040c41fc23deb75c6cf00eef9
George Gecewicz <gecewicz.george@gmail.com>
2016-09-26 18:12:12 -0400

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -12,63 +12,63 @@ defined( 'WPINC' ) or die;
 
 class Tribe__Extension__Order_List_Widget_Events_by_Publish_Date {
 
-    /**
-     * The semantic version number of this extension; should always match the plugin header.
-     */
-    const VERSION = '1.0.0';
+	/**
+	 * The semantic version number of this extension; should always match the plugin header.
+	 */
+	const VERSION = '1.0.0';
 
-    /**
-     * Each plugin required by this extension
-     *
-     * @var array Plugins are listed in 'main class' => 'minimum version #' format
-     */
-    public $plugins_required = array(
-        'Tribe__Events__Main' => '4.2'
-    );
+	/**
+	 * Each plugin required by this extension
+	 *
+	 * @var array Plugins are listed in 'main class' => 'minimum version #' format
+	 */
+	public $plugins_required = array(
+		'Tribe__Events__Main' => '4.2'
+	);
 
-    /**
-     * Parameters that let you target specific widget instances.
-     *
-     * @var array
-     */
-    protected $constraints = array(
+	/**
+	 * Parameters that let you target specific widget instances.
+	 *
+	 * @var array
+	 */
+	protected $constraints = array(
 		'sidebar_id'   => null,
 		'widget_id'    => null,
 		'widget_title' => null
 	);
 
-    /**
-     * The constructor; delays initializing the extension until all other plugins are loaded.
-     */
-    public function __construct( array $constraints = array() ) {
+	/**
+	 * The constructor; delays initializing the extension until all other plugins are loaded.
+	 */
+	public function __construct( array $constraints = array() ) {
 
-    	$this->constraints = array_merge( $this->constraints, $constraints );
+		$this->constraints = array_merge( $this->constraints, $constraints );
 
-        add_action( 'plugins_loaded', array( $this, 'init' ), 100 );
-    }
+		add_action( 'plugins_loaded', array( $this, 'init' ), 100 );
+	}
 
-    /**
-     * Extension hooks and initialization; exits if the extension is not authorized by Tribe Common to run.
-     */
-    public function init() {
+	/**
+	 * Extension hooks and initialization; exits if the extension is not authorized by Tribe Common to run.
+	 */
+	public function init() {
 
-        // Exit early if our framework is saying this extension should not run.
-        if ( ! function_exists( 'tribe_register_plugin' ) || ! tribe_register_plugin( __FILE__, __CLASS__, self::VERSION, $this->plugins_required ) ) {
-            return;
-        }
+		// Exit early if our framework is saying this extension should not run.
+		if ( ! function_exists( 'tribe_register_plugin' ) || ! tribe_register_plugin( __FILE__, __CLASS__, self::VERSION, $this->plugins_required ) ) {
+		    return;
+		}
 
-        add_filter( 'widget_display_callback', array( $this, 'setup' ), 10, 3 );
-    }
+		add_filter( 'widget_display_callback', array( $this, 'setup' ), 10, 3 );
+	}
 
-    /**
-     * Set up the ordering overwrites on widget instances.
-     *
-     * @param array $instance
-     * @param object $widget
-     * @param array $args
-     * @return array
-     */
-    public function setup( $instance, $widget, $args ) {
+	/**
+	 * Set up the ordering overwrites on widget instances.
+	 *
+	 * @param array $instance
+	 * @param object $widget
+	 * @param array $args
+	 * @return array
+	 */
+	public function setup( $instance, $widget, $args ) {
 		
 		// We're interested only in the (advanced or regular) events list widgets.
 		$targets = array( 'tribe-events-adv-list-widget', 'tribe-events-list-widget' );
@@ -87,12 +87,12 @@ class Tribe__Extension__Order_List_Widget_Events_by_Publish_Date {
 	}
 
 	/**
-     * For the given widget instance, see if it meets the constraints supplied to target specific widgets.
-     *
-     * @param array $instance
-     * @param array $args
-     * @return array
-     */
+	 * For the given widget instance, see if it meets the constraints supplied to target specific widgets.
+	 *
+	 * @param array $instance
+	 * @param array $args
+	 * @return array
+	 */
 	protected function constraints_met( $instance, $args ) {
 		$fail = false;
 
@@ -112,11 +112,11 @@ class Tribe__Extension__Order_List_Widget_Events_by_Publish_Date {
 	}
 
 	/**
-     * Set up the alteration of the orderby parameters on allowed widget instances.
-     *
-     * @param array $args
-     * @return array
-     */
+	 * Set up the alteration of the orderby parameters on allowed widget instances.
+	 *
+	 * @param array $args
+	 * @return array
+	 */
 	public function order_by_latest( $args ) {
 
 		// Don't interfere in other queries.
@@ -129,11 +129,11 @@ class Tribe__Extension__Order_List_Widget_Events_by_Publish_Date {
 	}
 
 	/**
-     * Perform the alteration of the orderby parameters on allowed widget instances.
-     *
-     * @param array $args
-     * @return array
-     */
+	 * Perform the alteration of the orderby parameters on allowed widget instances.
+	 *
+	 * @param array $args
+	 * @return array
+	 */
 	public function override_orderby( $orderby_sql ) {
 		global $wpdb;
 
